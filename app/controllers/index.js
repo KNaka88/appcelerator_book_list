683a77dda81a58edbf128c7dd9c97ff09d342a0f
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-10-31 14:53:20 -0700

    add backbutton for ios

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,13 +6,23 @@ var book = Alloy.createModel('books', {
 });
 
 function showBook(event) {
+	
     var selectedBook = event.source;
+    
     var args = {
         title: selectedBook.title,
         author: selectedBook.author
     };
+    
     var bookview = Alloy.createController("bookdetails", args).getView();
-    bookview.open();
+    
+    if (OS_IOS) {
+    	$.index.openWindow(bookview);
+    }
+    
+    if (OS_ANDROID) {
+    	bookview.open();
+    }    
 }      
 
 
